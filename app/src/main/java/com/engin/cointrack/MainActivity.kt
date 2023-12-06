6e4d0bcde3965d6c01f51f435388dae3d0081d88
Emre Engin <engiinemre@gmail.com>
2023-12-06 12:47:15 +0300

    Initial commit

---
+++ app/src/main/java/com/engin/cointrack/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.engin.cointrack
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.engin.cointrack.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeNavigation()
+    }
+
+    private fun initializeNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.favouriteFragment,R.id.homeFragment))
+        setupWithNavController(binding.bottomNavigationView,navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+
+}
