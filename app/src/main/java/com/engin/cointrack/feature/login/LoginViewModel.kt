24a3ada5aa21fb3b1b55b0bb26b9766b55119319
enginemre <emre.engin@getir.com>
2023-12-08 21:41:30 +0300

    Add login usecase

--- app/src/main/java/com/engin/cointrack/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/engin/cointrack/feature/login/LoginViewModel.kt
@@ -1,22 +1,24 @@
 package com.engin.cointrack.feature.login
 
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import androidx.lifecycle.viewModelScope
+import com.engin.cointrack.feature.login.domain.LoginUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val firebaseAuth: FirebaseAuth
+    private val loginUserUseCase: LoginUserUseCase
 ) : ViewModel() {
-
 
     private val _isLoading = MutableStateFlow(false)
     val isLoading = _isLoading.asStateFlow()
@@ -27,23 +29,18 @@ class LoginViewModel @Inject constructor(
     private val _loggedIn = Channel<Unit>()
     val loggedIn = _loggedIn.receiveAsFlow()
 
-
     fun login(email: String, password: String) {
-        if (email.isBlank()){
-            _error.trySend("Email couldn't be empty")
-            return
-        }
-        if (password.isBlank()){
-            _error.trySend("Password Couldn't be empty")
-            return
-        }
-        _isLoading.update { true }
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
-            _isLoading.update { false }
-            _loggedIn.trySend(Unit)
-        }.addOnFailureListener {
-            _isLoading.update { false }
-            _error.trySend(it.message.toString())
-        }
+        loginUserUseCase(email, password)
+            .onStart {
+                _isLoading.update { true }
+            }.onEach {
+                _loggedIn.trySend(Unit)
+                _isLoading.update { false }
+            }
+            .catch {
+                _error.send("")
+                _isLoading.update { false }
+            }
+            .launchIn(viewModelScope)
     }
 }
